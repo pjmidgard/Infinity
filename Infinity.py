df81e95d6201bbf18a01369857e7814a3adc67aa
Java Software <pj.midgard@yahoo.com>
2022-10-18 20:57:35 +0100

    Update Infinity.py

--- Infinity.py
+++ Infinity.py
@@ -1,7 +1,3 @@
-#Algorithm Infinity:
-
-#We should take 64 bits and find two the same #variations for 0-4 bits long and 0-32 bit, then, #save where it is a 3 bits variation, 8 combination #and 3 bits variation, 8 combination and find #variation should be random. information should #move inside on the file.
-
 from time import time
 cvf=0
 import os
